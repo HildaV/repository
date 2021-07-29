427985328e2e69dfb599c54c83c4c516615b040c
HILDA <HILDA@LAPTOP-VA1BID4N>
2021-07-28 19:54:04 -0500

    Add Ejercicios

---
+++ AutomationQA/src/test/java/com/java/CondicionalStatement.java
@@ -0,0 +1,27 @@
+package com.java;
+
+public class CondicionalStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int velocidad =60;//km/h
+		int limitevelocidad=80;
+		
+		if(velocidad> limitevelocidad) {
+			
+			System.out.println("El coche va a exceso de velocidad, Multa ");
+			
+		}else {
+			System.out.println("El coche va velocidad a la velocidad adecuada, Felicidades ");
+			
+			
+		}
+		
+		//IF ANIDADO   IF ELSE IF
+		
+		boolean carretera= true;
+
+	}
+
+}
---
+++ AutomationQA/src/test/java/com/java/Multiplicacion.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class Multiplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int entero1 = 2;
+		int entero2 = 3;
+		int entero3 = 4;
+		int mult;
+		
+		mult = entero1*entero2*entero3;
+		System.out.println("La Multiplicacion es:" + mult);
+		
+		
+
+	}
+
+}
