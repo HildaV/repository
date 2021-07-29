0a07e2838b34e78ff27c4eed188c6200ddaa037e
HILDA <HILDA@LAPTOP-VA1BID4N>
2021-07-28 20:25:45 -0500

    Multiplicacion

--- AutomationQA/src/test/java/com/java/Multiplicacion.java
+++ AutomationQA/src/test/java/com/java/Multiplicacion.java
@@ -5,13 +5,8 @@ public class Multiplicacion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int entero1 = 2;
-		int entero2 = 3;
-		int entero3 = 4;
-		int mult;
-		
-		mult = entero1*entero2*entero3;
-		System.out.println("La Multiplicacion es:" + mult);
+		int entero1 = 2,entero2 = 3 ,entero3 = 4;
+		System.out.println("La Multiplicacion es:" + entero1*entero2*entero3);
 		
 		
 
